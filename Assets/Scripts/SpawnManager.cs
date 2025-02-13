cde11ed7811e5c855f462603a90dd036207e6051
CGTurks <112087464+CGTurks@users.noreply.github.com>
2025-02-13 19:10:33 +0800

    set

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+
+    private float spawnRangeX = 10; // Horizontal range for top spawns
+    private float spawnPosZ = 20; // Z position for top spawns
+
+    private float spawnPosX = 20; // X position for left & right spawns
+    private float spawnRangeZ = 10; // Vertical range for left & right spawns
+
     private float startDelay = 2;
-    
     private float spawnInterval = 1.5f;
-    // Start is called before the first frame update
+
     void Start()
     {
-        InvokeRepeating("EnemySpawner", startDelay, spawnInterval);
+        InvokeRepeating("TopEnemySpawner", startDelay, spawnInterval);
+        InvokeRepeating("LeftEnemySpawner", startDelay, spawnInterval);
+        InvokeRepeating("RightEnemySpawner", startDelay, spawnInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    void TopEnemySpawner()
     {
-       
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.identity);
     }
-    void  EnemySpawner() {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-            0, spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-            animalPrefabs[animalIndex].transform.rotation);
-        
+
+    void LeftEnemySpawner()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(-spawnPosX, 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(0, 90, 0)); // Face right
+    }
+
+    void RightEnemySpawner()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(spawnPosX, 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(0, -90, 0)); // Face left
     }
 }
