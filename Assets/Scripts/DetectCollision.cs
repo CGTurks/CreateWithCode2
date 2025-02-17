ad6f0ef3875ec7f98722c1552b07d6d41f73f727
CGTurks <112087464+CGTurks@users.noreply.github.com>
2025-02-17 08:19:10 +0800

    set

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -1,13 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int health = 3; 
+
     void OnTriggerEnter(Collider other)
- {
-    Destroy(gameObject);
-    Destroy(other.gameObject);
- }
- }
+    {
+        GameManager gameManager = FindObjectOfType<GameManager>();
+
+        if (other.CompareTag("Player"))
+        {
+            
+            if (gameManager != null)
+            {
+                gameManager.PlayerHit();
+            }
+
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Projectile"))
+        {
+           
+            health--;
+
+            if (health <= 0)
+            {
+               
+                if (gameManager != null)
+                {
+                    gameManager.IncreaseScore();
+                }
+
+                Destroy(gameObject); 
+            }
+
+            Destroy(other.gameObject); 
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,40 +1,78 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement; 
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameOverText;
 
     private int lives = 3;
     private int score = 0;
+    private bool isGameOver = false;
 
     void Start()
     {
+        gameOverText.gameObject.SetActive(false); 
         UpdateUI();
+    }
+
+    void Update()
+    {
+        if (isGameOver && Input.GetKeyDown(KeyCode.R)) 
+        {
+            RestartGame();
+        }
     }
 
     public void IncreaseScore()
     {
-        score++;
-        UpdateUI();
+        if (!isGameOver)
+        {
+            score++;
+            UpdateUI();
+        }
     }
 
     public void DecreaseLives()
     {
-        lives--;
-        UpdateUI();
-
-        if (lives <= 0)
+        if (!isGameOver)
         {
-            Debug.Log("Game Over");
+            lives--;
+            UpdateUI();
+
+            if (lives <= 0)
+            {
+                GameOver();
+            }
         }
     }
 
     private void UpdateUI()
     {
         scoreText.text = "Score: " + score;
+        Debug.Log("Score: " + score);
         livesText.text = "Lives: " + lives;
+        Debug.Log("Lives: " + lives);
+    }
+
+    public void PlayerHit()
+    {
+        DecreaseLives();
+    }
+
+    private void GameOver()
+    {
+        isGameOver = true;
+        gameOverText.gameObject.SetActive(true); 
+        Debug.Log("Game Over");
+        Time.timeScale = 0f; 
+    }
+
+    private void RestartGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
     }
 }
-
