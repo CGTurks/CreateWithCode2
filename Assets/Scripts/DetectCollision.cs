eea1a27286779320eb30f0ee4a5a7550efa3ccfa
CGTurks <112087464+CGTurks@users.noreply.github.com>
2025-02-20 02:05:16 +0800

    set

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI; // Import UI namespace
 
 public class DetectCollision : MonoBehaviour
 {
-    public int health = 3; 
+    public int health = 3;
+    public Slider hungerBar; // Reference to the Hunger Bar UI
+
+    void Start()
+    {
+        if (hungerBar != null)
+        {
+            hungerBar.maxValue = health;
+            hungerBar.value = 0; // Start empty
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -10,7 +21,6 @@ public class DetectCollision : MonoBehaviour
 
         if (other.CompareTag("Player"))
         {
-            
             if (gameManager != null)
             {
                 gameManager.PlayerHit();
@@ -20,21 +30,24 @@ public class DetectCollision : MonoBehaviour
         }
         else if (other.CompareTag("Projectile"))
         {
-           
             health--;
+
+            if (hungerBar != null)
+            {
+                hungerBar.value += 1; // Fill up the hunger bar
+            }
 
             if (health <= 0)
             {
-               
                 if (gameManager != null)
                 {
                     gameManager.IncreaseScore();
                 }
 
-                Destroy(gameObject); 
+                Destroy(gameObject);
             }
 
-            Destroy(other.gameObject); 
+            Destroy(other.gameObject);
         }
     }
 }
